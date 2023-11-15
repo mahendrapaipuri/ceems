889db7a64ff6ecc9700e1468aba16983d6bb6fd2
mahendrapaipuri <mahendra.paipuri@gmail.com>
2023-11-15 17:05:45 +0100

    ci: Check metrics names

---
+++ checkmetrics.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+if [[ ( -z "$1" ) || ( -z "$2" ) ]]; then
+    echo "usage: ./checkmetrics.sh /usr/bin/promtool e2e-test-output.txt"
+    exit 1
+fi
+
+# Ignore known issues in auto-generated and network specific collectors.
+lint=$($1 check metrics < "$2" 2>&1 | grep -v -E "^batchjob_(memory_fail_count|memsw_fail_count)")
+
+if [[ -n $lint ]]; then
+    echo -e "Some Prometheus metrics do not follow best practices:\n"
+    echo "$lint"
+
+    exit 1
+fi
