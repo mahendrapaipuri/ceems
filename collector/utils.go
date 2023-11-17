dc87cfd2c2c56876a84529721b69d31611676ef8
mahendrapaipuri <mahendra.paipuri@gmail.com>
2023-11-17 12:10:16 +0100

    feat: Add emissions collector
    - Use RTE eCO2 mix for real time emission data for France
    
    - Use global energy data for constant emission factor for other countries
    
    Signed-off-by: mahendrapaipuri <mahendra.paipuri@gmail.com>

--- collector/utils.go
+++ collector/utils.go
@@ -1,13 +1,21 @@
-//go:build !nostat
-// +build !nostat
-
 package collector
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
 	"os"
 	"path/filepath"
 	"strconv"
 	"strings"
+	"time"
+)
+
+const (
+	OPENDATASOFT_API_PATH    = `%s/api/records/1.0/search/?%s`
+	OPENDATASOFT_API_BASEURL = `https://odre.opendatasoft.com`
 )
 
 // Load cgroups v2 metrics from a given path
@@ -43,4 +51,39 @@ func LoadCgroupsV2Metrics(name string, controllers []string) (map[string]float64
 		}
 	}
 	return data, nil
+}
+
+// Request to OPENDATASOFT API to get RTE energy data for France
+func GetRteEnergyMixData() (float64, error) {
+	params := url.Values{}
+	params.Add("dataset", "eco2mix-national-tr")
+	params.Add("facet", "nature")
+	params.Add("facet", "date_heure")
+	params.Add("start", "0")
+	params.Add("rows", "1")
+	params.Add("sort", "date_heure")
+	params.Add("q", fmt.Sprintf("date_heure:[%s TO #now()] AND NOT #null(taux_co2)", time.Now().Format("2006-01-02")))
+	queryString := params.Encode()
+
+	resp, err := http.DefaultClient.Get(fmt.Sprintf(OPENDATASOFT_API_PATH, OPENDATASOFT_API_BASEURL, queryString))
+	if err != nil {
+		return -1, err
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return -1, err
+	}
+
+	var data nationalRealTimeResponse
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		return -1, err
+	}
+
+	var fields []nationalRealTimeFields
+	for _, r := range data.Records {
+		fields = append(fields, r.Fields)
+	}
+	return float64(fields[0].TauxCo2), nil
 }
