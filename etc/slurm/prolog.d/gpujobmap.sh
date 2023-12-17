1fc9c5174b423f937c96b94b70a2c71724481d9d
Mahendra Paipuri <mahendra.paipuri@gmail.com>
2023-12-17 21:16:13 +0100

    chore: Add example config files
    Signed-off-by: Mahendra Paipuri <mahendra.paipuri@gmail.com>

---
+++ etc/slurm/prolog.d/gpujobmap.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Need to use this path in --collector.nvidia.gpu.job.map.path flag for batchjob_exporter
+DEST=/run/gpujobmap
+[ -e $DEST ] || mkdir -m 755 $DEST
+
+# CUDA_VISIBLE_DEVICES in prolog will be "actual" GPU indices and once job starts
+# CUDA will reset the indices to always start from 0. Thus inside a job, CUDA_VISIBLE_DEVICES
+# will always start with 0 but during prolog script execution it can be any ordinal index
+# based on how SLURM allocated the GPUs
+# Ref: https://slurm.schedmd.com/prolog_epilog.html
+for i in ${GPU_DEVICE_ORDINAL//,/ } ${CUDA_VISIBLE_DEVICES//,/ }; do
+  echo $SLURM_JOB_ID > $DEST/$i
+done
+exit 0 
---
+++ etc/slurm/prolog.d/slurmjobprops.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Need to use this path in --collector.slurm.job.props.path flag for batchjob_exporter
+DEST=/run/slurmjobprops
+[ -e $DEST ] || mkdir -m 755 $DEST
+
+# Important to keep the order as SLURM_JOB_UID SLURM_JOB_ACCOUNT SLURM_JOB_NODELIST
+echo $SLURM_JOB_UID $SLURM_JOB_ACCOUNT $SLURM_JOB_NODELIST > $DEST/$SLURM_JOB_ID
+exit 0 
