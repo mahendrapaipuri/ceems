25c18d277599644d7b169ae7d143051226db6e2f
Mahendra Paipuri <mahendra.paipuri@gmail.com>
2023-12-20 14:35:06 +0100

    refactor: Simplify ipmi collector config
    * Try different privilege esclation modes to detect the one that works
    
    * No need to pass any CLI flags to configure IPMI collector apart from command to run
    
    Signed-off-by: Mahendra Paipuri <mahendra.paipuri@gmail.com>

--- pkg/collector/ipmi.go
+++ pkg/collector/ipmi.go
@@ -23,6 +23,7 @@ const ipmiCollectorSubsystem = "ipmi_dcmi"
 
 type impiCollector struct {
 	logger          log.Logger
+	execMode        string
 	wattsMetricDesc *prometheus.Desc
 }
 
@@ -31,10 +32,10 @@ var (
 		"collector.ipmi.dcmi.cmd",
 		"IPMI DCMI command to get system power statistics. Use full path to executables.",
 	).Default("/usr/sbin/ipmi-dcmi --get-system-power-statistics").String()
-	ipmiDcmiExecAsRoot = kingpin.Flag(
-		"collector.ipmi.dcmi.exec.run.as.root",
-		"Execute IPMI DCMI command as root. This requires the current process to have CAP_SET(UID,GID) capabilities.",
-	).Default("false").Bool()
+	// ipmiDcmiExecAsRoot = kingpin.Flag(
+	// 	"collector.ipmi.dcmi.exec.run.as.root",
+	// 	"Execute IPMI DCMI command as root. This requires the current process to have CAP_SET(UID,GID) capabilities.",
+	// ).Default("false").Bool()
 	// ipmiDcmiExecWithSudo = kingpin.Flag(
 	// 	"collector.ipmi.dcmi.exec.run.with.sudo",
 	// 	"Execute IPMI DCMI command with sudo. This requires the current has sudo privileges on command set in --collector.ipmi.dcmi.cmd.",
@@ -59,8 +60,37 @@ func NewIpmiCollector(logger log.Logger) (Collector, error) {
 		"Current Power consumption in watts", []string{}, nil,
 	)
 
+	var execMode string = ""
+
+	// Split command
+	cmdSlice := strings.Split(*ipmiDcmiCmd, " ")
+
+	// Verify if running ipmiDcmiCmd works
+	if _, err := helpers.Execute(cmdSlice[0], cmdSlice[1:], logger); err == nil {
+		execMode = "native"
+		goto outside
+	}
+
+	// If ipmiDcmiCmd failed to run and if sudo is not already present in command,
+	// add sudo to command and execute. If current user has sudo rights it will be a success
+	if cmdSlice[0] != "sudo" {
+		if _, err := helpers.ExecuteWithTimeout("sudo", cmdSlice, 2, logger); err == nil {
+			execMode = "sudo"
+			goto outside
+		}
+	}
+
+	// As last attempt, run the command as root user by forking subprocess
+	// as root. If there is setuid cap on the process, it will be a success
+	if _, err := helpers.ExecuteAs(cmdSlice[0], cmdSlice[1:], 0, 0, logger); err == nil {
+		execMode = "cap"
+		goto outside
+	}
+
+outside:
 	collector := impiCollector{
 		logger:          logger,
+		execMode:        execMode,
 		wattsMetricDesc: wattsMetricDesc,
 	}
 	return &collector, nil
@@ -90,19 +120,24 @@ func (c *impiCollector) Update(ch chan<- prometheus.Metric) error {
 
 	// Execute ipmi-dcmi command
 	cmdSlice := strings.Split(*ipmiDcmiCmd, " ")
-	if *ipmiDcmiExecAsRoot {
+	if c.execMode == "cap" {
 		stdOut, err = helpers.ExecuteAs(cmdSlice[0], cmdSlice[1:], 0, 0, c.logger)
-	} else {
+	} else if c.execMode == "sudo" {
+		stdOut, err = helpers.ExecuteWithTimeout("sudo", cmdSlice, 1, c.logger)
+	} else if c.execMode == "native" {
 		stdOut, err = helpers.Execute(cmdSlice[0], cmdSlice[1:], c.logger)
+	} else {
+		err = fmt.Errorf("Current process do not have permissions to execute %s", *ipmiDcmiCmd)
 	}
 	if err != nil {
+		level.Error(c.logger).Log("msg", "Failed to collect IPMI DCMI data", "error", err)
 		return err
 	}
 
 	// Parse power consumption from output
 	currentPowerConsumption, err := c.getCurrentPowerConsumption(stdOut)
 	if err != nil {
-		level.Error(c.logger).Log("msg", "Failed to collect IPMI DCMI data", "error", err)
+		level.Error(c.logger).Log("msg", "Failed to parse IPMI DCMI command output", "error", err)
 		return err
 	}
 
