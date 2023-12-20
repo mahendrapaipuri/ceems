64920aaf397ce84c9844747a88a1a728e65f84a4
Mahendra Paipuri <mahendra.paipuri@gmail.com>
2023-12-20 14:40:54 +0100

    refactor: Simplify slurm sacct getter
    * Try different methods to get sacct output and choose one that works during init
    
    * Filter jobs that have a minimum elapsed time
    
    * Remove unnecessary CLI args
    
    * Update tests
    
    Signed-off-by: Mahendra Paipuri <mahendra.paipuri@gmail.com>

--- pkg/jobstats/slurm_test.go
+++ pkg/jobstats/slurm_test.go
@@ -8,9 +8,10 @@ import (
 )
 
 var (
-	sacctCmdOutput = `JobID|Partition|Account|Group|GID|User|UID|Submit|Start|End|Elapsed|ExitCode|State|NNodes|NodeList|JobName|WorkDir
-1479763|part1|acc1|grp|1000|usr|1000|2023-02-21T14:37:02|2023-02-21T14:37:07|2023-02-21T15:26:29|00:49:22|0:0|CANCELLED by 302137|1|compute-0|test_script1|/home/usr
-1481508|part1|acc1|grp|1000|usr|1000|2023-02-21T15:48:20|2023-02-21T15:49:06|2023-02-21T15:57:23|00:08:17|0:0|CANCELLED by 302137|2|compute-[0-2]|test_script2|/home/usr`
+	sacctCmdOutput = `JobID|Partition|Account|Group|GID|User|UID|Submit|Start|End|Elapsed|ElapsedRaw|ExitCode|State|NNodes|NodeList|JobName|WorkDir
+1479763|part1|acc1|grp|1000|usr|1000|2023-02-21T14:37:02|2023-02-21T14:37:07|2023-02-21T15:26:29|00:49:22|3000|0:0|CANCELLED by 302137|1|compute-0|test_script1|/home/usr
+1481508|part1|acc1|grp|1000|usr|1000|2023-02-21T15:48:20|2023-02-21T15:49:06|2023-02-21T15:57:23|00:08:17|4920|0:0|CANCELLED by 302137|2|compute-[0-2]|test_script2|/home/usr
+1481510|part1|acc1|grp|1000|usr|1000|2023-02-21T15:48:20|2023-02-21T15:49:06|2023-02-21T15:57:23|00:00:17|17|0:0|CANCELLED by 302137|2|compute-[0-2]|test_script2|/home/usr`
 	logger            = log.NewNopLogger()
 	expectedBatchJobs = []BatchJob{
 		{
@@ -51,11 +52,12 @@ var (
 			Nodelist:    "compute-[0-2]",
 			NodelistExp: "compute-0|compute-1|compute-2",
 		},
+		{}, // Ignored jobs will have empty struct
 	}
 )
 
 func TestParseSacctCmdOutput(t *testing.T) {
-	batchJobs, numJobs := parseSacctCmdOutput(sacctCmdOutput, logger)
+	batchJobs, numJobs := parseSacctCmdOutput(sacctCmdOutput, 60, logger)
 	if !reflect.DeepEqual(batchJobs, expectedBatchJobs) {
 		t.Errorf("Expected batch jobs %#v. \n\nGot %#v", expectedBatchJobs, batchJobs)
 	}
