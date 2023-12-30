17d10f798462709aa9faaa97b6f8c90dcf70a4ba
Mahendra Paipuri <mahendra.paipuri@gmail.com>
2023-12-30 18:26:19 +0100

    test: Update tests and fixtures
    Signed-off-by: Mahendra Paipuri <mahendra.paipuri@gmail.com>

--- scripts/checkmetrics.sh
+++ scripts/checkmetrics.sh
@@ -6,7 +6,7 @@ if [[ ( -z "$1" ) || ( -z "$2" ) ]]; then
 fi
 
 # Ignore known issues in auto-generated and network specific collectors.
-lint=$($1 check metrics < "$2" 2>&1 | grep -v -E "^batchjob_(memory_fail_count|memsw_fail_count)")
+lint=$($1 check metrics < "$2" 2>&1 | grep -v -E "^batchjob_slurm_job_(memory_fail_count|memsw_fail_count)")
 
 if [[ -n $lint ]]; then
     echo -e "Some Prometheus metrics do not follow best practices:\n"
