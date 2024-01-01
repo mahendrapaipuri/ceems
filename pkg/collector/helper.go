3cc5c836f66cf0ed4201000bf14db31eb9583837
Mahendra Paipuri <mahendra.paipuri@gmail.com>
2024-01-01 12:23:10 +0100

    feat: Rework slurm collector
    * Merge GPU jobID map with slurm collector. This is more logical organization instead of having a separate collector
    
    * Dont report swap and PSI metrics by default. They can be enabled  using CLI flag
    
    * Add a hidden flag to force cgroups version for testing
    
    * Refacorting of certain receivers to be more clean
    
    * Add more unit tests to cover more scenarios
    
    * Add test fixtures to be able to unit test
    
    * Add more e2e test scenarios
    
    Signed-off-by: Mahendra Paipuri <mahendra.paipuri@gmail.com>

--- pkg/collector/helper.go
+++ pkg/collector/helper.go
@@ -7,7 +7,18 @@ import (
 	"strings"
 
 	"regexp"
+
+	"github.com/go-kit/log"
+	"github.com/go-kit/log/level"
+	"github.com/mahendrapaipuri/batchjob_monitoring/internal/helpers"
 )
+
+type Device struct {
+	index string
+	name  string
+	uuid  string
+	isMig bool
+}
 
 var (
 	metricNameRegex = regexp.MustCompile(`_*[^0-9A-Za-z_]+_*`)
@@ -74,4 +85,65 @@ func LoadCgroupsV2Metrics(
 		}
 	}
 	return data, nil
+}
+
+// Get all physical or MIG devices using nvidia-smi command
+// Example output:
+// bash-4.4$ nvidia-smi --query-gpu=name,uuid --format=csv
+// name, uuid
+// Tesla V100-SXM2-32GB, GPU-f124aa59-d406-d45b-9481-8fcd694e6c9e
+// Tesla V100-SXM2-32GB, GPU-61a65011-6571-a6d2-5ab8-66cbb6f7f9c3
+//
+// Here we are using nvidia-smi to avoid having build issues if we use
+// nvml go bindings. This way we dont have deps on nvidia stuff and keep
+// exporter simple.
+//
+// NOTE: Hoping this command returns MIG devices too
+func GetNvidiaGPUDevices(nvidiaSmiPath string, logger log.Logger) (map[string]Device, error) {
+	// Check if nvidia-smi binary exists
+	if _, err := os.Stat(nvidiaSmiPath); err != nil {
+		level.Error(logger).Log("msg", "Failed to open nvidia-smi executable", "path", nvidiaSmiPath, "err", err)
+		return nil, err
+	}
+
+	// Execute nvidia-smi command to get available GPUs
+	args := []string{"--query-gpu=index,name,uuid", "--format=csv"}
+	nvidiaSmiOutput, err := helpers.Execute(nvidiaSmiPath, args, logger)
+	if err != nil {
+		level.Error(logger).
+			Log("msg", "nvidia-smi command to get list of devices failed", "err", err)
+		return nil, err
+	}
+
+	// Get all devices
+	gpuDevices := map[string]Device{}
+	for _, line := range strings.Split(string(nvidiaSmiOutput), "\n") {
+		// Header line, empty line and newlines are ignored
+		if line == "" || line == "\n" || strings.HasPrefix(line, "index") {
+			continue
+		}
+
+		devDetails := strings.Split(line, ",")
+		if len(devDetails) < 3 {
+			level.Error(logger).
+				Log("msg", "Cannot parse output from nvidia-smi command", "output", line)
+			continue
+		}
+
+		// Get device index, name and UUID
+		devIndx := strings.TrimSpace(devDetails[0])
+		devName := strings.TrimSpace(devDetails[1])
+		devUuid := strings.TrimSpace(devDetails[2])
+
+		// Check if device is in MiG mode
+		isMig := false
+		if strings.HasPrefix(devUuid, "MIG") {
+			isMig = true
+		}
+		level.Debug(logger).
+			Log("msg", "Found nVIDIA GPU", "name", devName, "UUID", devUuid, "isMig:", isMig)
+
+		gpuDevices[devIndx] = Device{index: devIndx, name: devName, uuid: devUuid, isMig: isMig}
+	}
+	return gpuDevices, nil
 }
--- pkg/collector/slurm_test.go
+++ pkg/collector/slurm_test.go
@@ -6,6 +6,7 @@ package collector
 import (
 	"fmt"
 	"reflect"
+	"strconv"
 	"testing"
 
 	"github.com/go-kit/log"
@@ -13,18 +14,29 @@ import (
 
 var expectedSlurmMetrics CgroupMetric
 
+func mockGPUDevices() map[string]Device {
+	var devs = make(map[string]Device, 4)
+	for i := 0; i <= 4; i++ {
+		idxString := strconv.Itoa(i)
+		devs[idxString] = Device{index: idxString, uuid: fmt.Sprintf("GPU-%d", i)}
+	}
+	return devs
+}
+
 func TestCgroupsV2SlurmJobMetrics(t *testing.T) {
 	if _, err := BatchJobExporterApp.Parse(
 		[]string{
 			"--path.cgroupfs", "fixtures/sys/fs/cgroup",
 			"--collector.slurm.create.unique.jobids",
 			"--collector.slurm.job.props.path", "fixtures/slurmjobprops",
+			"--collector.slurm.nvidia.gpu.job.map.path", "fixtures/gpujobmap",
 		},
 	); err != nil {
 		t.Fatal(err)
 	}
 	c := slurmCollector{
 		cgroups:          "v2",
+		nvidiaGPUDevs:    mockGPUDevices(),
 		cgroupsRootPath:  *cgroupfsPath,
 		slurmCgroupsPath: fmt.Sprintf("%s/system.slice/slurmstepd.scope", *cgroupfsPath),
 		logger:           log.NewNopLogger(),
@@ -51,19 +63,21 @@ func TestCgroupsV2SlurmJobMetrics(t *testing.T) {
 		jobaccount:      "testacc",
 		jobid:           "1009248",
 		jobuuid:         "ac28caf5-ce6c-35f6-73fb-47d9d43f7780",
+		jobGpuOrdinals:  []string{"2", "3"},
 		step:            "",
 		task:            "",
 		batch:           "slurm",
-		err:             false}
+		err:             false,
+	}
 	if err != nil {
 		t.Fatalf("Cannot fetch data from getJobsMetrics function: %v ", err)
 	}
 	if !reflect.DeepEqual(metrics["1009248"], expectedSlurmMetrics) {
-		t.Fatalf("Expected metrics data is %+v: \nGot %+v", expectedSlurmMetrics, metrics)
+		t.Fatalf("Expected metrics data is %+v: \nGot %+v", expectedSlurmMetrics, metrics["1009248"])
 	}
 }
 
-func TestCgroupsV2WithProcFsSlurmJobMetrics(t *testing.T) {
+func TestCgroupsV2SlurmJobMetricsWithProcFs(t *testing.T) {
 	if _, err := BatchJobExporterApp.Parse(
 		[]string{
 			"--path.cgroupfs", "fixtures/sys/fs/cgroup",
@@ -76,6 +90,7 @@ func TestCgroupsV2WithProcFsSlurmJobMetrics(t *testing.T) {
 	c := slurmCollector{
 		cgroups:          "v2",
 		cgroupsRootPath:  *cgroupfsPath,
+		nvidiaGPUDevs:    mockGPUDevices(),
 		slurmCgroupsPath: fmt.Sprintf("%s/system.slice/slurmstepd.scope", *cgroupfsPath),
 		logger:           log.NewNopLogger(),
 	}
@@ -101,15 +116,68 @@ func TestCgroupsV2WithProcFsSlurmJobMetrics(t *testing.T) {
 		jobaccount:      "testacc",
 		jobid:           "1009248",
 		jobuuid:         "ac28caf5-ce6c-35f6-73fb-47d9d43f7780",
+		jobGpuOrdinals:  []string{"2", "3"},
 		step:            "",
 		task:            "",
 		batch:           "slurm",
-		err:             false}
+		err:             false,
+	}
 	if err != nil {
 		t.Fatalf("Cannot fetch data from getJobsMetrics function: %v ", err)
 	}
 	if !reflect.DeepEqual(metrics["1009248"], expectedSlurmMetrics) {
-		t.Fatalf("Expected metrics data is %+v: \nGot %+v", expectedSlurmMetrics, metrics)
+		t.Fatalf("Expected metrics data is %+v: \nGot %+v", expectedSlurmMetrics, metrics["1009248"])
+	}
+}
+
+func TestCgroupsV2SlurmJobMetricsNoJobProps(t *testing.T) {
+	if _, err := BatchJobExporterApp.Parse(
+		[]string{
+			"--path.cgroupfs", "fixtures/sys/fs/cgroup",
+			"--collector.slurm.create.unique.jobids",
+		},
+	); err != nil {
+		t.Fatal(err)
+	}
+	c := slurmCollector{
+		cgroups:          "v2",
+		cgroupsRootPath:  *cgroupfsPath,
+		nvidiaGPUDevs:    mockGPUDevices(),
+		slurmCgroupsPath: fmt.Sprintf("%s/system.slice/slurmstepd.scope", *cgroupfsPath),
+		logger:           log.NewNopLogger(),
+	}
+	metrics, err := c.getJobsMetrics()
+	expectedSlurmMetrics = CgroupMetric{
+		name:            "/system.slice/slurmstepd.scope/job_1009248",
+		cpuUser:         60375.292848,
+		cpuSystem:       115.777502,
+		cpuTotal:        60491.070351,
+		cpus:            2,
+		cpuPressure:     0,
+		memoryRSS:       4.098592768e+09,
+		memoryCache:     0,
+		memoryUsed:      4.111491072e+09,
+		memoryTotal:     4.294967296e+09,
+		memoryFailCount: 0,
+		memswUsed:       0,
+		memswTotal:      1.8446744073709552e+19,
+		memswFailCount:  0,
+		memoryPressure:  0,
+		userslice:       false,
+		jobuid:          "",
+		jobaccount:      "",
+		jobid:           "1009248",
+		jobuuid:         "a0523e93-a037-c2b1-8b34-410c9996399c",
+		step:            "",
+		task:            "",
+		batch:           "slurm",
+		err:             false,
+	}
+	if err != nil {
+		t.Fatalf("Cannot fetch data from getJobsMetrics function: %v ", err)
+	}
+	if !reflect.DeepEqual(metrics["1009248"], expectedSlurmMetrics) {
+		t.Fatalf("Expected metrics data is %+v: \nGot %+v", expectedSlurmMetrics, metrics["1009248"])
 	}
 }
 
@@ -117,6 +185,7 @@ func TestCgroupsV1SlurmJobMetrics(t *testing.T) {
 	if _, err := BatchJobExporterApp.Parse(
 		[]string{
 			"--path.cgroupfs", "fixtures/sys/fs/cgroup",
+			"--path.procfs", "fixtures/proc",
 			"--collector.slurm.create.unique.jobids",
 			"--collector.slurm.job.props.path", "fixtures/slurmjobprops",
 		},
@@ -126,6 +195,7 @@ func TestCgroupsV1SlurmJobMetrics(t *testing.T) {
 	c := slurmCollector{
 		cgroups:          "v1",
 		logger:           log.NewNopLogger(),
+		nvidiaGPUDevs:    mockGPUDevices(),
 		cgroupsRootPath:  fmt.Sprintf("%s/cpuacct", *cgroupfsPath),
 		slurmCgroupsPath: fmt.Sprintf("%s/cpuacct/slurm", *cgroupfsPath),
 	}
@@ -151,14 +221,16 @@ func TestCgroupsV1SlurmJobMetrics(t *testing.T) {
 		jobaccount:      "testacc",
 		jobid:           "1009248",
 		jobuuid:         "ac28caf5-ce6c-35f6-73fb-47d9d43f7780",
+		jobGpuOrdinals:  []string{"2", "3"},
 		step:            "",
 		task:            "",
 		batch:           "slurm",
-		err:             false}
+		err:             false,
+	}
 	if err != nil {
 		t.Fatalf("Cannot fetch data from getJobsMetrics function: %v ", err)
 	}
 	if !reflect.DeepEqual(metrics["1009248"], expectedSlurmMetrics) {
-		t.Fatalf("Expected metrics data is %+v: \nGot %+v", expectedSlurmMetrics, metrics)
+		t.Fatalf("Expected metrics data is %+v: \nGot %+v", expectedSlurmMetrics, metrics["1009248"])
 	}
 }
