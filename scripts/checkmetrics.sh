f8eedd1c3c2c7bc319cbb51e1752e6401b81f9f9
Mahendra Paipuri <mahendra.paipuri@gmail.com>
2024-01-06 18:34:52 +0100

    test: Fix check metrics script
    * Ensure we lint all output files
    
    Signed-off-by: Mahendra Paipuri <mahendra.paipuri@gmail.com>

--- scripts/checkmetrics.sh
+++ scripts/checkmetrics.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
 
 if [[ ( -z "$1" ) || ( -z "$2" ) ]]; then
-    echo "usage: ./checkmetrics.sh /usr/bin/promtool e2e-test-output.txt"
+    echo "usage: ./checkmetrics.sh /usr/bin/promtool output_dir"
     exit 1
 fi
 
 # Ignore known issues in auto-generated and network specific collectors.
-lint=$($1 check metrics < "$2" 2>&1 | grep -v -E "^batchjob_slurm_job_(memory_fail_count|memsw_fail_count)")
+search_dir="$2"
+for entry in "$search_dir"/*
+do
+  lint=$($1 check metrics < "$entry" 2>&1 | grep -v -E "^batchjob_slurm_job_(memory_fail_count|memsw_fail_count)")
 
-if [[ -n $lint ]]; then
-    echo -e "Some Prometheus metrics do not follow best practices:\n"
-    echo "$lint"
+  if [[ -n $lint ]]; then
+      echo -e "Some Prometheus metrics do not follow best practices:\n"
+      echo "$lint"
 
-    exit 1
-fi
+      exit 1
+  fi
+done
+
