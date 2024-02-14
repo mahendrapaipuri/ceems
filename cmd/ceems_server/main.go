11dae72200faaf8238196619a9f262521a144e7d
Mahendra Paipuri <mahendra.paipuri@gmail.com>
2024-02-14 15:02:06 +0100

    fix: Unignore cmd packages
    Signed-off-by: Mahendra Paipuri <mahendra.paipuri@gmail.com>

---
+++ cmd/ceems_server/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/mahendrapaipuri/ceems/pkg/stats/cli"
+)
+
+// Main entry point for `ceems` app
+func main() {
+	// Create a new app
+	CEEMSServer, err := cli.NewCEEMSServer()
+	if err != nil {
+		panic("Failed to create an instance of CEEMS Server App")
+	}
+
+	// Main entrypoint of the app
+	if err := CEEMSServer.Main(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
---
+++ cmd/ceems_server/main_test.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"testing"
+	"time"
+)
+
+var (
+	binary, _ = filepath.Abs("../../bin/ceems_server")
+)
+
+const (
+	address = "localhost:19020"
+)
+
+func TestBatchjobStatsExecutable(t *testing.T) {
+	fmt.Println(binary)
+	if _, err := os.Stat(binary); err != nil {
+		t.Errorf("ceems_server binary not available, try to run `make build` first: %s", err)
+	}
+	tmpDir := t.TempDir()
+	tmpSacctPath := tmpDir + "/sacct"
+
+	sacctPath, err := filepath.Abs("../../pkg/stats/fixtures/sacct")
+	if err != nil {
+		t.Error(err)
+	}
+	err = os.Link(sacctPath, tmpSacctPath)
+	if err != nil {
+		t.Error(err)
+	}
+
+	usagestats := exec.Command(
+		binary, "--path.data", tmpDir,
+		"--slurm.sacct.path", tmpSacctPath,
+		"--resource.manager.slurm",
+		"--web.listen-address", address,
+	)
+	if err := runCommandAndTests(usagestats); err != nil {
+		t.Error(err)
+	}
+}
+
+func runCommandAndTests(cmd *exec.Cmd) error {
+	if err := cmd.Start(); err != nil {
+		return fmt.Errorf("failed to start command: %s", err)
+	}
+
+	// Sleep for a while and kill process
+	time.Sleep(1 * time.Second)
+
+	cmd.Process.Kill()
+	return nil
+}
