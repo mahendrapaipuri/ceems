c9011d5e78931e1b8e44052bbc10e0e68218a77c
Mahendra Paipuri <mahendra.paipuri@gmail.com>
2024-02-15 10:48:46 +0100

    feat: Add support for different IPMI implmts
    * Besides FreeIPMI, we support IPMITool and IPMIUtil
    
    * Fix metric type and set it to guage instead of counter
    
    * Export average power consumption metric too
    
    Signed-off-by: Mahendra Paipuri <mahendra.paipuri@gmail.com>

--- pkg/collector/ipmi.go
+++ pkg/collector/ipmi.go
@@ -28,33 +28,89 @@ type impiCollector struct {
 	metricDesc   map[string]*prometheus.Desc
 }
 
-// Expected output from DCMI spec
-// Ref: https://www.intel.com/content/dam/www/public/us/en/documents/technical-specifications/dcmi-v1-5-rev-spec.pdf
-// Current Power                        : 164 Watts
-// Minimum Power over sampling duration : 48 watts
-// Maximum Power over sampling duration : 361 watts
-// Average Power over sampling duration : 157 watts
-// Time Stamp                           : 12/29/2023 - 08:58:00
-// Statistics reporting time period     : 1473439000 milliseconds
-// Power Measurement                    : Active
+/*
+	Expected output from DCMI spec
+	Ref: https://www.intel.com/content/dam/www/public/us/en/documents/technical-specifications/dcmi-v1-5-rev-spec.pdf
+
+	Different IPMI implementations report stats in different formats:
+
+	**FreeIPMI**:
+	Command: `ipmi-dcmi --get-system-power-statistics`
+	Output:
+	----------------------------------------------------------------------
+	Current Power                        : 164 Watts
+	Minimum Power over sampling duration : 48 watts
+	Maximum Power over sampling duration : 361 watts
+	Average Power over sampling duration : 157 watts
+	Time Stamp                           : 12/29/2023 - 08:58:00
+	Statistics reporting time period     : 1473439000 milliseconds
+	Power Measurement                    : Active
+	----------------------------------------------------------------------
+
+	**OpenIPMI**:
+	Command: `ipmitool dcmi power reading`
+	Output:
+	----------------------------------------------------------------------
+
+		Instantaneous power reading:                    50 Watts
+		Minimum during sampling period:                  6 Watts
+		Maximum during sampling period:                304 Watts
+		Average power reading over sample period:       49 Watts
+		IPMI timestamp:                           Thu Feb 15 08:36:01 2024
+		Sampling period:                          00000001 Seconds.
+		Power reading state is:                   activated
+
+	----------------------------------------------------------------------
+	**IPMIUtils**:
+	Command: `ipmiutil dcmi power`
+	Output:
+	----------------------------------------------------------------------
+	ipmiutil dcmi ver 3.17
+	-- BMC version 6.10, IPMI version 2.0
+	DCMI Version:                   1.5
+	DCMI Power Management:          Supported
+	DCMI System Interface Access:   Supported
+	DCMI Serial TMode Access:       Supported
+	DCMI Secondary LAN Channel:     Supported
+	Current Power:                   49 Watts
+	Min Power over sample duration:  6 Watts
+	Max Power over sample duration:  304 Watts
+	Avg Power over sample duration:  49 Watts
+	Timestamp:                       Thu Feb 15 09:37:32 2024
+
+	Sampling period:                 1000 ms
+	Power reading state is:          active
+	Exception Action:  OEM defined
+	Power Limit:       896 Watts (inactive)
+	Correction Time:   62914560 ms
+	Sampling period:   1472 sec
+	ipmiutil dcmi, completed successfully
+	----------------------------------------------------------------------
+*/
 
 var (
 	ipmiDcmiCmd = CEEMSExporterApp.Flag(
 		"collector.ipmi.dcmi.cmd",
 		"IPMI DCMI command to get system power statistics. Use full path to executables.",
 	).Default("/usr/sbin/ipmi-dcmi --get-system-power-statistics").String()
+	// IPMIUtil Ref: https://sourceforge.net/p/ipmiutil/code-git/ci/master/tree/util/idcmi.c#l343
+	// FreeIPMI Ref: https://github.com/chu11/freeipmi-mirror/blob/f4057a93cbc11ecf4bfadb4bcd5d375f65f01f19/ipmi-dcmi/ipmi-dcmi.c#L1828-L1830
+	// IPMITool Ref: https://github.com/ipmitool/ipmitool/blob/be11d948f89b10be094e28d8a0a5e8fb532c7b60/lib/ipmi_dcmi.c#L1447-L1451
 	ipmiDCMIPowerMeasurementRegex = regexp.MustCompile(
-		`^Power Measurement\s*:\s*(?P<value>Active|Not\sAvailable).*`,
+		`^\s*(?:Power Measurement|Power reading state is)\s*:\s*(?P<value>[A|a]ctive|Not\sAvailable|not\sactive|activated|deactivated).*`,
 	)
 	ipmiDCMIPowerReadingRegexMap = map[string]*regexp.Regexp{
 		"current": regexp.MustCompile(
-			`^Current Power\s*:\s*(?P<value>[0-9.]*)\s*[w|W]atts.*`,
+			`^\s*(?:Current Power|Instantaneous power reading)\s*:\s*(?P<value>[0-9.]*)\s*[w|W]atts.*`,
 		),
 		"min": regexp.MustCompile(
-			`^Minimum Power over sampling duration\s*:\s*(?P<value>[0-9.]*)\s*[w|W]atts.*`,
+			`^\s*(?:Minimum Power over sampling duration|Minimum during sampling period|Min Power over sample duration)\s*:\s*(?P<value>[0-9.]*)\s*[w|W]atts.*`,
 		),
 		"max": regexp.MustCompile(
-			`^Maximum Power over sampling duration\s*:\s*(?P<value>[0-9.]*)\s*[w|W]atts.*`,
+			`^\s*(?:Maximum Power over sampling duration|Maximum during sampling period|Max Power over sample duration)\s*:\s*(?P<value>[0-9.]*)\s*[w|W]atts.*`,
+		),
+		"avg": regexp.MustCompile(
+			`^\s*(?:Average Power over sampling duration|Average power reading over sample period|Avg Power over sample duration)\s*:\s*(?P<value>[0-9.]*)\s*[w|W]atts.*`,
 		),
 	}
 )
@@ -71,16 +127,20 @@ func NewIPMICollector(logger log.Logger) (Collector, error) {
 	var metricDesc = make(map[string]*prometheus.Desc, 3)
 	var cachedMetric = make(map[string]float64, 3)
 	metricDesc["current"] = prometheus.NewDesc(
-		prometheus.BuildFQName(Namespace, ipmiCollectorSubsystem, "current_watts_total"),
+		prometheus.BuildFQName(Namespace, ipmiCollectorSubsystem, "current_watts"),
 		"Current Power consumption in watts", []string{"hostname"}, nil,
 	)
 	metricDesc["min"] = prometheus.NewDesc(
-		prometheus.BuildFQName(Namespace, ipmiCollectorSubsystem, "min_watts_total"),
+		prometheus.BuildFQName(Namespace, ipmiCollectorSubsystem, "min_watts"),
 		"Minimum Power consumption in watts", []string{"hostname"}, nil,
 	)
 	metricDesc["max"] = prometheus.NewDesc(
-		prometheus.BuildFQName(Namespace, ipmiCollectorSubsystem, "max_watts_total"),
+		prometheus.BuildFQName(Namespace, ipmiCollectorSubsystem, "max_watts"),
 		"Maximum Power consumption in watts", []string{"hostname"}, nil,
+	)
+	metricDesc["avg"] = prometheus.NewDesc(
+		prometheus.BuildFQName(Namespace, ipmiCollectorSubsystem, "avg_watts"),
+		"Average Power consumption in watts", []string{"hostname"}, nil,
 	)
 
 	// Split command
@@ -152,7 +212,7 @@ func (c *impiCollector) Update(ch chan<- prometheus.Metric) error {
 	// Returned value 0 means Power Measurement is not avail
 	for rType, rValue := range powerReadings {
 		if rValue > 0 {
-			ch <- prometheus.MustNewConstMetric(c.metricDesc[rType], prometheus.CounterValue, float64(rValue), c.hostname)
+			ch <- prometheus.MustNewConstMetric(c.metricDesc[rType], prometheus.GaugeValue, float64(rValue), c.hostname)
 			c.cachedMetric[rType] = rValue
 		}
 	}
@@ -185,7 +245,7 @@ func (c *impiCollector) parseIPMIOutput(stdOut []byte) (map[string]float64, erro
 
 	// When Power Measurement in 'Active' state - we can get watts
 	var powerReadings = make(map[string]float64, 3)
-	if value == "Active" {
+	if value == "active" || value == "Active" || value == "activated" {
 		// Get power readings
 		for rType, regex := range ipmiDCMIPowerReadingRegexMap {
 			if reading, err := getValue(stdOut, regex); err == nil {
--- pkg/collector/ipmi_test.go
+++ pkg/collector/ipmi_test.go
@@ -11,7 +11,8 @@ import (
 )
 
 var (
-	ipmidcmiStdout = `
+	ipmidcmiStdout = map[string]string{
+		"freeipmi": `
 Current Power                        : 332 Watts
 Minimum Power over sampling duration : 68 watts
 Maximum Power over sampling duration : 504 watts
@@ -19,8 +20,7 @@ Average Power over sampling duration : 348 watts
 Time Stamp                           : 11/03/2023 - 08:36:29
 Statistics reporting time period     : 2685198000 milliseconds
 Power Measurement                    : Active
-`
-	ipmidcmiStdoutAlt = `
+`, "freeipmiAlt": `
 Current Power                        : 332 watts
 Minimum Power over sampling duration : 68 Watts
 Maximum Power over sampling duration : 504 Watts
@@ -28,43 +28,70 @@ Average Power over sampling duration : 348 Watts
 Time Stamp                           : 11/03/2023 - 08:36:29
 Statistics reporting time period     : 2685198000 milliseconds
 Power Measurement                    : Active
-`
-	ipmidcmiStdoutDisactive = `
-Power Measurement                    : Disable
-`
+`, "ipmitutil": `
+ipmiutil dcmi ver 3.17
+-- BMC version 6.10, IPMI version 2.0
+DCMI Version:                   1.5
+DCMI Power Management:          Supported
+DCMI System Interface Access:   Supported
+DCMI Serial TMode Access:       Supported
+DCMI Secondary LAN Channel:     Supported
+Current Power:                   332 Watts
+Min Power over sample duration:  68 Watts
+Max Power over sample duration:  504 Watts
+Avg Power over sample duration:  348 Watts
+Timestamp:                       Thu Feb 15 09:37:32 2024
+
+Sampling period:                 1000 ms
+Power reading state is:          active
+Exception Action:  OEM defined
+Power Limit:       896 Watts (inactive)
+Correction Time:   62914560 ms
+Sampling period:   1472 sec
+ipmiutil dcmi, completed successfully
+`, "ipmitool": `
+
+	Instantaneous power reading:                   332 Watts
+	Minimum during sampling period:                 68 Watts
+	Maximum during sampling period:                504 Watts
+	Average power reading over sample period:      348 Watts
+	IPMI timestamp:                           Thu Feb 15 08:36:01 2024
+	Sampling period:                          00000001 Seconds.
+	Power reading state is:                   activated
+
+`}
+	ipmidcmiStdoutDisactive = map[string]string{
+		"freeipmi":  "Power Measurement                    : Not Available",
+		"ipmitutil": "Power reading state is:          not active",
+		"ipmitool":  "Power reading state is:                   deactivated",
+	}
 	expectedPower = map[string]float64{
 		"current": 332,
 		"min":     68,
 		"max":     504,
+		"avg":     348,
 	}
 )
 
 func TestIpmiMetrics(t *testing.T) {
 	c := impiCollector{logger: log.NewNopLogger()}
-	value, err := c.parseIPMIOutput([]byte(ipmidcmiStdout))
-	if err != nil {
-		t.Errorf("failed to parse IPMI DCMI output: %v", err)
-	}
-	if !reflect.DeepEqual(value, expectedPower) {
-		t.Fatalf("expected power %v. Got %v", expectedPower, value)
-	}
-}
-
-func TestIpmiMetricsAlt(t *testing.T) {
-	c := impiCollector{logger: log.NewNopLogger()}
-	value, err := c.parseIPMIOutput([]byte(ipmidcmiStdoutAlt))
-	if err != nil {
-		t.Errorf("failed to parse IPMI DCMI output: %v", err)
-	}
-	if !reflect.DeepEqual(value, expectedPower) {
-		t.Fatalf("expected power %v. Got %v", expectedPower, value)
+	for testName, testString := range ipmidcmiStdout {
+		value, err := c.parseIPMIOutput([]byte(testString))
+		if err != nil {
+			t.Errorf("failed to parse %s output: %v", testName, err)
+		}
+		if !reflect.DeepEqual(value, expectedPower) {
+			t.Fatalf("%s: expected power %v. Got %v", testName, expectedPower, value)
+		}
 	}
 }
 
 func TestIpmiMetricsDisactive(t *testing.T) {
 	c := impiCollector{logger: log.NewNopLogger()}
-	value, _ := c.parseIPMIOutput([]byte(ipmidcmiStdoutDisactive))
-	if value != nil {
-		t.Errorf("Expected nil output. Got %v", value)
+	for testName, testString := range ipmidcmiStdoutDisactive {
+		value, _ := c.parseIPMIOutput([]byte(testString))
+		if value != nil {
+			t.Errorf("%s: expected nil output. Got %v", testName, value)
+		}
 	}
 }
