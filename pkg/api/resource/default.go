03a262aabb0ab14d6101a4fe2b26b2855aea9f73
Mahendra Paipuri <mahendra.paipuri@gmail.com>
2024-03-23 20:55:07 +0100

    feat: Add a default resource manager
    * A fallback option just to start CEEMS API server when no resource manager is selected
    
    * Default resource manager returns empty jobs and so DB will be empty
    
    Signed-off-by: Mahendra Paipuri <mahendra.paipuri@gmail.com>

---
+++ pkg/api/resource/default.go
@@ -0,0 +1,33 @@
+package resource
+
+import (
+	"time"
+
+	"github.com/go-kit/log"
+	"github.com/go-kit/log/level"
+	"github.com/mahendrapaipuri/ceems/pkg/api/models"
+)
+
+// defaultResourceManager struct
+type defaultResourceManager struct {
+	logger log.Logger
+}
+
+func init() {
+	// Register resource manager
+	RegisterManager("default", NewDefaultResourceManager)
+}
+
+// NewDefaultResourceManager returns a new defaultResourceManager that returns empty compute units
+func NewDefaultResourceManager(logger log.Logger) (Fetcher, error) {
+	level.Info(logger).Log("msg", "Dummy resource manager activated")
+	return &defaultResourceManager{
+		logger: logger,
+	}, nil
+}
+
+// Return empty units response
+func (d *defaultResourceManager) Fetch(start time.Time, end time.Time) ([]models.Unit, error) {
+	level.Info(d.logger).Log("msg", "Empty units fetched from default resource manager")
+	return []models.Unit{}, nil
+}
