5665d027f1ea9c236b26da1a4ab8762b01ce0c6f
Mahendra Paipuri <mahendra.paipuri@gmail.com>
2024-07-13 19:03:49 +0200

    fix: Correct avging metric maps
    * Add more tests to validate all cases
    
    * Add unit tests that verifies DB custom funcs
    
    Signed-off-by: Mahendra Paipuri <mahendra.paipuri@gmail.com>

--- pkg/api/db/sqlite3/sqlite3_test.go
+++ pkg/api/db/sqlite3/sqlite3_test.go
@@ -8,6 +8,7 @@ import (
 	"path/filepath"
 	"testing"
 
+	"github.com/mahendrapaipuri/ceems/pkg/api/models"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -156,7 +157,7 @@ func TestAvgMetricMap(t *testing.T) {
 			new:            `{"a":4,"c":2}`,
 			existingWeight: 1,
 			newWeight:      2,
-			expected:       `{"a":3,"b":1,"c":4}`,
+			expected:       `{"a":3,"b":3,"c":4}`,
 		},
 		{
 			name:           "when new has invalid types",
@@ -182,11 +183,43 @@ func TestAvgMetricMap(t *testing.T) {
 			newWeight:      2,
 			expected:       `{"a":3,"b":0,"c":2}`,
 		},
+		{
+			name:           "when existing is empty",
+			existing:       `{}`,
+			new:            `{"a":4,"c":3,"b":1}`,
+			existingWeight: 1,
+			newWeight:      2,
+			expected:       `{"a":4,"b":1,"c":3}`,
+		},
+		{
+			name:           "when new is empty",
+			existing:       `{"a":4,"c":3,"b":1}`,
+			new:            `{}`,
+			existingWeight: 1,
+			newWeight:      2,
+			expected:       `{"a":4,"b":1,"c":3}`,
+		},
+		{
+			name:           "when existing weight is 0",
+			existing:       `{"a":4,"c":3,"b":1}`,
+			new:            `{"a":4,"c":2,"b":1}`,
+			existingWeight: 0,
+			newWeight:      2,
+			expected:       `{"a":4,"b":1,"c":2}`,
+		},
+		{
+			name:           "when new weight is 0",
+			existing:       `{"a":4,"c":3,"b":1}`,
+			new:            `{"a":4,"c":2,"b":1}`,
+			existingWeight: 1,
+			newWeight:      0,
+			expected:       `{"a":4,"b":1,"c":3}`,
+		},
 	}
 
 	for _, test := range tests {
 		got := avgMetricMap(test.existing, test.new, test.existingWeight, test.newWeight)
-		assert.Equal(t, test.expected, got)
+		assert.Equal(t, test.expected, got, test.name)
 	}
 }
 
@@ -223,4 +256,223 @@ func TestAvgMetricMapAgg(t *testing.T) {
 	// Finally do the aggregation
 	aggMap := gMap.Done()
 	assert.Equal(t, expectedMap, aggMap)
+}
+
+func setupDB(tmpDir string, aggMetric bool, units []models.Unit) (models.MetricMap, models.MetricMap, error) {
+	dbPath := filepath.Join(tmpDir, "test.db")
+	db, err := sql.Open(DriverName, dbPath)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to create DB")
+	}
+
+	stmts := `
+PRAGMA foreign_keys=OFF;
+BEGIN TRANSACTION;
+CREATE TABLE units (
+	"id" integer not null primary key,
+	"uuid" text,
+	"total_time_seconds" text,
+	"avg_cpu_usage" text
+);
+CREATE UNIQUE INDEX uq_cluster_id_uuid_start ON units (uuid);`
+	_, err = db.Exec(stmts)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to create table in DB: %s", err)
+	}
+
+	updateStmt := `
+INSERT INTO units (uuid,total_time_seconds,avg_cpu_usage) VALUES(:uuid,:total_time_seconds,:avg_cpu_usage) ON CONFLICT(uuid) DO UPDATE SET
+  total_time_seconds = add_metric_map(total_time_seconds, :total_time_seconds),
+  avg_cpu_usage = avg_metric_map(avg_cpu_usage, :avg_cpu_usage, CAST(json_extract(total_time_seconds, '$.alloc_cputime') AS REAL), CAST(json_extract(:total_time_seconds, '$.alloc_cputime') AS REAL))`
+
+	sqlStmt, err := db.Prepare(updateStmt)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to prepare statement for table %s", err)
+	}
+
+	for _, unit := range units {
+		if _, err := sqlStmt.Exec(
+			sql.Named("uuid", unit.UUID),
+			sql.Named("total_time_seconds", unit.TotalTime),
+			sql.Named("avg_cpu_usage", unit.AveCPUUsage),
+		); err != nil {
+			return nil, nil, fmt.Errorf("failed to insert data for table %s", err)
+		}
+	}
+
+	// Make units query
+	var cpuUsage, totalTimes models.MetricMap
+	if aggMetric {
+		_ = db.QueryRow("SELECT avg_metric_map_agg(avg_cpu_usage, CAST(json_extract(total_time_seconds, '$.alloc_cputime') AS REAL)) AS avg_cpu_usage, sum_metric_map_agg(total_time_seconds) AS total_time_seconds FROM units").Scan(&cpuUsage, &totalTimes)
+	} else {
+		_ = db.QueryRow("SELECT avg_cpu_usage, total_time_seconds FROM units").Scan(&cpuUsage, &totalTimes)
+	}
+	return cpuUsage, totalTimes, nil
+}
+
+func TestCustomFuncsInDB(t *testing.T) {
+	tests := []struct {
+		name               string
+		aggMetric          bool
+		units              []models.Unit
+		expectedCPUUsage   models.MetricMap
+		expectedTotalTimes models.MetricMap
+	}{
+		{
+			name: "first update",
+			units: []models.Unit{
+				{
+					UUID:        "1",
+					TotalTime:   models.MetricMap{"alloc_cputime": 1000},
+					AveCPUUsage: models.MetricMap{"global": 10},
+				},
+			},
+			expectedCPUUsage:   models.MetricMap{"global": 10},
+			expectedTotalTimes: models.MetricMap{"alloc_cputime": 1000},
+		},
+		{
+			name: "second update",
+			units: []models.Unit{
+				{
+					UUID:        "1",
+					TotalTime:   models.MetricMap{"alloc_cputime": 1000},
+					AveCPUUsage: models.MetricMap{"global": 10},
+				},
+				{
+					UUID:        "1",
+					TotalTime:   models.MetricMap{"alloc_cputime": 500},
+					AveCPUUsage: models.MetricMap{"global": 25},
+				},
+			},
+			expectedCPUUsage:   models.MetricMap{"global": 15},
+			expectedTotalTimes: models.MetricMap{"alloc_cputime": 1500},
+		},
+		{
+			name: "second update with null update value",
+			units: []models.Unit{
+				{
+					UUID:        "1",
+					TotalTime:   models.MetricMap{"alloc_cputime": 1000},
+					AveCPUUsage: models.MetricMap{"global": 10},
+				},
+				{
+					UUID:        "1",
+					TotalTime:   models.MetricMap{"alloc_cputime": 500},
+					AveCPUUsage: models.MetricMap{},
+				},
+			},
+			expectedCPUUsage:   models.MetricMap{"global": 10},
+			expectedTotalTimes: models.MetricMap{"alloc_cputime": 1500},
+		},
+		{
+			name: "second update with zero update value",
+			units: []models.Unit{
+				{
+					UUID:        "1",
+					TotalTime:   models.MetricMap{"alloc_cputime": 1000},
+					AveCPUUsage: models.MetricMap{"global": 10},
+				},
+				{
+					UUID:        "1",
+					TotalTime:   models.MetricMap{"alloc_cputime": 1000},
+					AveCPUUsage: models.MetricMap{"global": 0},
+				},
+			},
+			expectedCPUUsage:   models.MetricMap{"global": 5},
+			expectedTotalTimes: models.MetricMap{"alloc_cputime": 2000},
+		},
+		{
+			name: "second update with zero weight value",
+			units: []models.Unit{
+				{
+					UUID:        "1",
+					TotalTime:   models.MetricMap{"alloc_cputime": 1000},
+					AveCPUUsage: models.MetricMap{"global": 10},
+				},
+				{
+					UUID:        "1",
+					TotalTime:   models.MetricMap{"alloc_cputime": 0},
+					AveCPUUsage: models.MetricMap{"global": 25},
+				},
+			},
+			expectedCPUUsage:   models.MetricMap{"global": 10},
+			expectedTotalTimes: models.MetricMap{"alloc_cputime": 1000},
+		},
+		{
+			name:      "aggregate query",
+			aggMetric: true,
+			units: []models.Unit{
+				{
+					UUID:        "1",
+					TotalTime:   models.MetricMap{"alloc_cputime": 1000},
+					AveCPUUsage: models.MetricMap{"global": 10},
+				},
+				{
+					UUID:        "2",
+					TotalTime:   models.MetricMap{"alloc_cputime": 500},
+					AveCPUUsage: models.MetricMap{"global": 25},
+				},
+				{
+					UUID:        "3",
+					TotalTime:   models.MetricMap{"alloc_cputime": 1500},
+					AveCPUUsage: models.MetricMap{"global": 5},
+				},
+			},
+			expectedCPUUsage:   models.MetricMap{"global": 10},
+			expectedTotalTimes: models.MetricMap{"alloc_cputime": 3000},
+		},
+		{
+			name:      "aggregate query with null metric",
+			aggMetric: true,
+			units: []models.Unit{
+				{
+					UUID:        "1",
+					TotalTime:   models.MetricMap{"alloc_cputime": 1000},
+					AveCPUUsage: models.MetricMap{"global": 10},
+				},
+				{
+					UUID:        "2",
+					TotalTime:   models.MetricMap{"alloc_cputime": 500},
+					AveCPUUsage: models.MetricMap{},
+				},
+				{
+					UUID:        "3",
+					TotalTime:   models.MetricMap{"alloc_cputime": 1500},
+					AveCPUUsage: models.MetricMap{"global": 5},
+				},
+			},
+			expectedCPUUsage:   models.MetricMap{"global": 7},
+			expectedTotalTimes: models.MetricMap{"alloc_cputime": 3000},
+		},
+		{
+			name:      "aggregate query with zero value",
+			aggMetric: true,
+			units: []models.Unit{
+				{
+					UUID:        "1",
+					TotalTime:   models.MetricMap{"alloc_cputime": 500},
+					AveCPUUsage: models.MetricMap{"global": 10},
+				},
+				{
+					UUID:        "2",
+					TotalTime:   models.MetricMap{"alloc_cputime": 500},
+					AveCPUUsage: models.MetricMap{"global": 0},
+				},
+				{
+					UUID:        "3",
+					TotalTime:   models.MetricMap{"alloc_cputime": 2000},
+					AveCPUUsage: models.MetricMap{"global": 20},
+				},
+			},
+			expectedCPUUsage:   models.MetricMap{"global": 15},
+			expectedTotalTimes: models.MetricMap{"alloc_cputime": 3000},
+		},
+	}
+
+	for _, test := range tests {
+		gotCPUUsage, gotTotalTimes, err := setupDB(t.TempDir(), test.aggMetric, test.units)
+		require.NoError(t, err)
+		assert.Equal(t, test.expectedCPUUsage, gotCPUUsage, test.name)
+		assert.Equal(t, test.expectedTotalTimes, gotTotalTimes, test.name)
+	}
 }
