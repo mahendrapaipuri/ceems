9fa2ce07dcbf42a775d4a4dda60641f0756bcfd3
Mahendra Paipuri <mahendra.paipuri@gmail.com>
2024-09-05 22:18:20 +0200

    ci: Check clang version before installing clang 18
    Signed-off-by: Mahendra Paipuri <mahendra.paipuri@gmail.com>

--- scripts/install_clang.sh
+++ scripts/install_clang.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -exo pipefail
 
+# Check if clang exists. If it exists, we need to ensure that it
+# is at least of version >= 18
+if [ -x "$(command -v clang)" ]; then
+    clang_major_version=$(clang -v 2>&1 | grep version | grep -o "[0-9]\+\.[0-9]\+\.[0-9]\+" | cut -d "." -f1)
+    if (( ${clang_major_version} >= 18 )); then
+        echo "clang >=18 already installed. Skipping installation...."
+        exit 0
+    fi
+fi
+
 # Setup sudo prefix
 SUDO=''
 if (( $EUID != 0 )); then
