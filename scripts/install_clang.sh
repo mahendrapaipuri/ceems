6f37264753d2a122ed4f09c463e5eac6eb86a28a
Mahendra Paipuri <mahendra.paipuri@gmail.com>
2024-09-05 18:37:41 +0200

    build: Use sudo prefix everywhere in clang install script
    Signed-off-by: Mahendra Paipuri <mahendra.paipuri@gmail.com>

--- scripts/install_clang.sh
+++ scripts/install_clang.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 set -exo pipefail
 
-# Install clang stable version dependencies
-apt-get update && apt-get install -y --no-install-recommends  \
-    wget lsb-release wget software-properties-common gnupg    \
-    && apt-get clean && rm -rf /var/lib/apt/lists/*
-
-# Install clang 18
+# Setup sudo prefix
 SUDO=''
 if (( $EUID != 0 )); then
     SUDO='sudo'
 fi
+
+# Install clang stable version dependencies
+$SUDO apt-get update && $SUDO apt-get install -y --no-install-recommends  \
+    wget lsb-release wget software-properties-common gnupg    \
+    && apt-get clean && rm -rf /var/lib/apt/lists/*
+
+# Install clang 18
 $SUDO bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
 
 # Create necessary symlinks
-ln -vsnf /usr/lib/llvm-18/bin/clang /usr/bin/clang
-ln -vsnf /usr/lib/llvm-18/bin/llc /usr/bin/llc
+$SUDO ln -vsnf /usr/lib/llvm-18/bin/clang /usr/bin/clang
+$SUDO ln -vsnf /usr/lib/llvm-18/bin/llc /usr/bin/llc
