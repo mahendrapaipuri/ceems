533ea858e7170fdd1cc57ae13285c3b14a330072
Mahendra Paipuri <mahendra.paipuri@gmail.com>
2024-10-14 12:05:49 +0200

    ci: Ensure llc symlink is created when clang >= 18 is found
    Signed-off-by: Mahendra Paipuri <mahendra.paipuri@gmail.com>

--- scripts/install_clang.sh
+++ scripts/install_clang.sh
@@ -4,20 +4,27 @@ set -exo pipefail
 # This script only works for Ubuntu derivates and it is meant to be
 # used in CI to install clang in golang builder containers.
 
+create_symlinks() {
+    echo "Creating symlinks"
+    $SUDO ln -vsnf /usr/lib/llvm-18/bin/clang /usr/bin/clang
+    $SUDO ln -vsnf /usr/lib/llvm-18/bin/llc /usr/bin/llc
+}
+
+# Setup sudo prefix
+SUDO=''
+if (( $EUID != 0 )); then
+    SUDO='sudo'
+fi
+
 # Check if clang exists. If it exists, we need to ensure that it
 # is at least of version >= 18
 if [ -x "$(command -v clang)" ]; then
     clang_major_version=$(clang -v 2>&1 | grep version | grep -o "[0-9]\+\.[0-9]\+\.[0-9]\+" | cut -d "." -f1)
     if (( ${clang_major_version} >= 18 )); then
         echo "clang >=18 already installed. Skipping installation...."
+        create_symlinks
         exit 0
     fi
-fi
-
-# Setup sudo prefix
-SUDO=''
-if (( $EUID != 0 )); then
-    SUDO='sudo'
 fi
 
 # Install clang stable version dependencies
@@ -29,5 +36,4 @@ $SUDO apt-get update && $SUDO apt-get install -y --no-install-recommends  \
 $SUDO bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
 
 # Create necessary symlinks
-$SUDO ln -vsnf /usr/lib/llvm-18/bin/clang /usr/bin/clang
-$SUDO ln -vsnf /usr/lib/llvm-18/bin/llc /usr/bin/llc
+create_symlinks
